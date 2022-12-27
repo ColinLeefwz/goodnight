3aad1c402b136212f3781ffe85eea48c2a418f3d
lolo lee <lilonglongfwz@gmail.com>
2022-12-27 12:12:05 +0900

    :sparkles: add following methods in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,34 @@ class User < ApplicationRecord
   has_many :passive_followings, class_name: 'Following', foreign_key: 'followed_id', dependent: :destroy
   has_many :followings, through: :active_followings, source: :followed
   has_many :followers, through: :passive_followings, source: :follower
+
+  # Judge if object user id is followed by current user
+  #
+  # @param id [Integer] object user id
+  #
+  # @return [Boolean] judgement result of following
+  def following?(id)
+    active_followings.exists?(followed_id: id)
+  end
+
+  # Follow the object user id
+  #
+  # @param id [Integer] object user id
+  #
+  # @rase [ActiveRecord::RecordInvalid] when failed validation
+  # @return [true] succeed in following object user
+  def follow!(id)
+    active_followings.create!(followed_id: id)
+  end
+
+  # Unfollow the object user id
+  #
+  # @param id [Integer] object user id
+  #
+  # @rase [ActiveRecord::RecordInvalid] when failed validation
+  # @return [true] succeed in unfollowing object user
+  def unfollow!(id)
+    active_following = active_followings.find_by(followed_id: id)
+    active_following.destroy!
+  end
 end
