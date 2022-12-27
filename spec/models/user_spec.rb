61eda46b26e155554de6305c94924df9492424a8
lolo lee <lilonglongfwz@gmail.com>
2022-12-27 12:12:05 +0900

    :sparkles: add following methods in user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,4 +16,43 @@ RSpec.describe User, type: :model do
 
     it { expect(user.followers).to match_array([follower_user]) }
   end
+
+  describe '#following?' do
+    let!(:user) { create(:user) }
+    let!(:followed_user) { create(:user) }
+    let!(:unfollowed_user) { create(:user) }
+    let!(:following) { create(:following, followed: followed_user, follower: user) }
+
+    it do
+      expect(user.following?(followed_user.id)).to be true
+      expect(user.following?(unfollowed_user.id)).to be false
+    end
+  end
+
+  describe '#follow!' do
+    let!(:user) { create(:user) }
+    let!(:expected_followed_user) { create(:user) }
+
+    subject { user.follow!(expected_followed_user.id) }
+
+    it 'follows user successfully' do
+      expect(user.reload.following?(expected_followed_user.id)).to be false
+      expect{ subject }.to change{ Following.count }.from(0).to(1)
+      expect(user.reload.following?(expected_followed_user.id)).to be true
+    end
+  end
+
+  describe '#unfollow!' do
+    let!(:user) { create(:user) }
+    let!(:expected_unfollowed_user) { create(:user) }
+    let!(:following) { create(:following, followed: expected_unfollowed_user, follower: user) }
+
+    subject { user.unfollow!(expected_unfollowed_user.id) }
+
+    it 'unfollows user successfully' do
+      expect(user.reload.following?(expected_unfollowed_user.id)).to be true
+      expect{ subject }.to change{ Following.count }.from(1).to(0)
+      expect(user.reload.following?(expected_unfollowed_user.id)).to be false
+    end
+  end
 end
