b6b20873370d3b850252b8fc13b6257ca253244e
lolo lee <lilonglongfwz@gmail.com>
2022-12-27 11:03:53 +0900

    :triangular_flag_on_post: add following model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :active_followings, class_name: 'Following', foreign_key: 'follower_id', dependent: :destroy
+  has_many :passive_followings, class_name: 'Following', foreign_key: 'followed_id', dependent: :destroy
+  has_many :followings, through: :active_followings, source: :followed
+  has_many :followers, through: :passive_followings, source: :follower
 end
