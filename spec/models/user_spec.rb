7dc30044a1bece0702d89e6a63475631d66e6ee6
lolo lee <lilonglongfwz@gmail.com>
2022-12-27 11:30:32 +0900

    :white_check_mark: add test cases for models
    - install test related gems
    - implement test cases

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#followings' do
+    let!(:user) { create(:user) }
+    let!(:followed_user) { create(:user) }
+    let!(:following) { create(:following, followed: followed_user, follower: user) }
+
+    it { expect(user.followings).to match_array([followed_user]) }
+  end
+
+  describe '#followers' do
+    let!(:user) { create(:user) }
+    let!(:follower_user) { create(:user) }
+    let!(:following) { create(:following, followed: user, follower: follower_user) }
+
+    it { expect(user.followers).to match_array([follower_user]) }
+  end
 end
